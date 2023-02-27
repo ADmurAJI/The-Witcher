62682a92b63374917239b0e2a397e2cbec04c8c7
Aleksandr Zautsev <alexandr.falkone@yandex.ru>
2023-02-27 23:43:50 +0300

    new file:   css/main.css 	new file:   img/screen-1/layer-back.jpg 	new file:   img/screen-1/layer-front.png 	new file:   img/screen-1/layer-middle.png 	new file:   img/screen-2/layer-back.jpg 	new file:   img/screen-2/layer-front.png 	new file:   img/screen-2/layer-gera.png 	new file:   img/screen-2/layer-yen.png 	new file:   img/screen-3/layer-back.jpg 	new file:   img/screen-3/layer-ciri.png 	new file:   img/screen-3/layer-front.png 	new file:   img/screen-3/layer-middle.png 	new file:   index.html 	new file:   js/app.js 	new file:   libs/particles/particles.js 	new file:   libs/swiper/swiper-bundle.min.css#	new file:   libs/swiper/swiper-bundle.min.js 	new file:   libs/swiper/swiper-bundle.min.js.map

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+  //direction: 'vertical',
+  //freeMode: true,
+  speed: 2500,
+  mousewheel: {
+    enabled: true,
+    sensitivity: 2.5,
+  },
+  spaceBetween: 15,
+  parallax: true,
+});
